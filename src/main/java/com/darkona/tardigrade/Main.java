c1423cb34d940904cc8fc51a6485ad6afe6b5a07
Javier Darkona <javier.darkona@gmail.com>
2023-12-24 21:13:20 -0300

    added rainbowify

--- src/main/java/com/darkona/tardigrade/Main.java
+++ src/main/java/com/darkona/tardigrade/Main.java
@@ -12,36 +12,52 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class Main {
 
-    static final String name = "Tardigrade";
-    static final String version = "1.0";
+    static final String NAME = "Tardigrade";
+    static final String VERSION = "1.0";
     static final String icon = "( ꒰֎꒱ )";
-    private static final String banner = "\u001B[1m\n (꒰֎ ꒱) \n උ( ___ )づ\n උ( ___ )づ \n  උ( ___ )づ\n උ( ___ )づ\u001B[0m";
-    private static final String nonUtfbanner = "\u001B[1m\n ( {∞} ) \n Ç( ___ )P\n Ç( ___ )P \n  Ç( ___ )P\n Ç( ___ )P\u001B[0m";
-    private static final String initMessage = name + " Server version " + version + " initializing.";
-
-    private static TardigradeConfiguration config;
-
-    static final String fullColor = Ansi.RED + "F"
-            + Ansi.ORANGE + "u"
-            + Ansi.YELLOW + "l"
-            + Ansi.GREEN + "l "
-            + Ansi.AQUA + "c"
-            + Ansi.BLUE + "o"
-            + Ansi.DARK_BLUE + "l"
-            + Ansi.PURPLE + "o"
-            + Ansi.PINK + "r" + Ansi.RESET;
-
     static final String monochrome = "░▒▓Monochrome▓▒░";
+    private static final String BANNER = "\u001B[1m\n (꒰֎ ꒱) \n උ( ___ )づ\n උ( ___ )づ \n  උ( ___ )づ\n උ( ___ )づ\u001B[0m";
+    private static final String NON_UTF_BANNER = "\u001B[1m\n ( {∞} ) \n Ç( ___ )P\n Ç( ___ )P \n  Ç( ___ )P\n Ç( ___ )P\u001B[0m";
+    private static final String INIT_MESSAGE = NAME + " Server version " + VERSION + " initializing.";
+    private static TardigradeConfiguration config;
+    static final String fullColor = rainbowify("Full color");
+
+
+    private static String rainbowify(String s) {
+        List<Ansi> rainbow = List.of(Ansi.RED, Ansi.ORANGE, Ansi.YELLOW, Ansi.GREEN, Ansi.AQUA, Ansi.BLUE, Ansi.PURPLE, Ansi.PINK);
+        StringBuilder result = new StringBuilder();
+        int colorIndex = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c != ' ' && c != '\n') {
+                result.append(rainbow.get(colorIndex));
+                colorIndex = (colorIndex + 1) % rainbow.size();
+            } else {
+                if (c == '\n') {
+                    colorIndex = 0;
+                }
+            }
+            result.append(c);
+        }
+        result.append(Ansi.RESET);
+        return result.toString();
+    }
+
+
     public static void main(String[] args) throws Exception {
         config = new TardigradeConfiguration(args);
         if (config.color() && !AnsiConsole.isInstalled() && System.console() != null) {
             AnsiConsole.systemInstall();
         }
-
         printInitialization(config);
 
         startserver();
@@ -62,16 +78,16 @@ public class Main {
 
     private static String name() {
         if (config.color()) {
-            return Ansi.PINK + name + Ansi.RESET;
+            return Ansi.PINK + NAME + Ansi.RESET;
         }
-        return name;
+        return NAME;
     }
 
     private static void printInitialization(TardigradeConfiguration config) throws Exception {
 
-        var actual_banner = detectUTF8() ? banner : nonUtfbanner;
+        var actual_banner = detectUTF8() ? BANNER : NON_UTF_BANNER;
         say(config.color() ? (Ansi.PINK + actual_banner + Ansi.RESET) : actual_banner);
-        say(initMessage);
+        say(INIT_MESSAGE);
         say((config.color() ? fullColor : monochrome) + " logging enabled.");
         var input = (config.color() ? Ansi.DARK_GREEN : "") + "/" + config.input() + Ansi.RESET;
         var output = (config.color() ? Ansi.DARK_RED : "") + "/" + config.output() + Ansi.RESET;
